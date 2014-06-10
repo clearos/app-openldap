fe95d794638b215c3b1522d6be1435fcbb647184
clearostranslations <translations@clearfoundation.com>
2014-06-09 22:26:33 -0400

    Tidied up translations

--- language/en_US/openldap_lang.php
+++ language/en_US/openldap_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['openldap_app_name'] = 'OpenLDAP Driver';
 $lang['openldap_app_description'] = 'The OpenLDAP Driver app provides the the necessary tools for users, groups, accounts and other directory services.';
-$lang['openldap_openldap_server'] = 'OpenLDAP Server';
-$lang['openldap_preparing_startup'] = 'Preparing startup...';
-$lang['openldap_importing_data'] = 'Importing data...';
-$lang['openldap_generating_configuration'] = 'Generating configuration...';
-$lang['openldap_preparing_system'] = 'Preparing system...';
+$lang['openldap_app_name'] = 'OpenLDAP Driver';
 $lang['openldap_directory_is_offline'] = 'Directory is offline.';
 $lang['openldap_directory_starting_up'] = 'Directory is starting up.';
+$lang['openldap_generating_configuration'] = 'Generating configuration...';
+$lang['openldap_importing_data'] = 'Importing data...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Preparing startup...';
+$lang['openldap_preparing_system'] = 'Preparing system...';
