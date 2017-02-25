0afbdcad69045a7d02c41ef44651614577896164
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:24:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = '두가지 OpenLDAP 드라이버 응용 프로그램은 사용자, 그룹, 계정 및 다른 디렉터리 서비스를 위해 필요한 도구를 제공합니다.';
+$lang['openldap_app_name'] = 'OpenLDAP를 드라이버';
+$lang['openldap_directory_is_offline'] = '디렉토리가 오프라인 상태입니다.';
+$lang['openldap_directory_starting_up'] = '디렉토리는 시작된다.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = '도메인 슬레이브 모드로 변경할 수 없다.';
+$lang['openldap_generating_configuration'] = '구성을 생성 ...';
+$lang['openldap_importing_data'] = '데이터를 가져 오는 중 ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP 서버';
+$lang['openldap_preparing_startup'] = '시작을 준비 중 ...';
+$lang['openldap_preparing_system'] = '시스템을 준비 중 ...';
