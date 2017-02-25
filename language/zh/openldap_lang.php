61f15aef80a2d5f2cfc843a25a8d37eadcb6c4dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:20:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'OpenLDAP的驱动程序为用户，组，帐户和其他目录服务的必要工具。';
+$lang['openldap_app_name'] = 'OpenLDAP的驱动程序';
+$lang['openldap_directory_is_offline'] = '目录处于脱机状态。';
+$lang['openldap_directory_starting_up'] = '目录正在启动。';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = '域不能在从模式进行更改。';
+$lang['openldap_generating_configuration'] = '生成的配置...';
+$lang['openldap_importing_data'] = '导入数据...';
+$lang['openldap_openldap_server'] = 'OpenLDAP服务器';
+$lang['openldap_preparing_startup'] = '准备启动...';
+$lang['openldap_preparing_system'] = '正在准备系统...';
