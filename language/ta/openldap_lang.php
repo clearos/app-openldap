61a21c051d084d35cdc0cccee1d3a4032ca4804c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:28:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'OpenLDAP ஆனது டிரைவர் பயன்பாட்டை பயனர்கள், குழுக்கள், கணக்குகள் மற்றும் பிற அடைவு சேவைகளுக்கு அவசியமான கருவிகள் வழங்குகிறது.';
+$lang['openldap_app_name'] = 'OpenLDAP ஆனது டிரைவர்';
+$lang['openldap_directory_is_offline'] = 'ஏற்கனவே ஆஃப்லைனில் உள்ளது.';
+$lang['openldap_directory_starting_up'] = 'ஏற்கனவே தொடங்கும்.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'டொமைன் அடிமை முறையில் மாற்ற முடியாது.';
+$lang['openldap_generating_configuration'] = 'கட்டமைப்பு உருவாக் ...';
+$lang['openldap_importing_data'] = 'தரவை இறக்குமதி ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP சேவையகம்';
+$lang['openldap_preparing_startup'] = 'தொடக்க தயாராகிறது ...';
+$lang['openldap_preparing_system'] = 'அமைப்பு தயாராகிறது ...';
