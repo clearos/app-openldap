f98a3f484cf13b98db952b24de5a2604c1e0315d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:25:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Ny OpenLDAP Driver fampiharana manome ny fitaovana ilaina ho an&#39;ny mpampiasa, vondrona, tantara sy ny asa Directory hafa.';
+$lang['openldap_app_name'] = 'OpenLDAP Driver';
+$lang['openldap_directory_is_offline'] = 'Directory dia ivelan&#39;ny aterineto.';
+$lang['openldap_directory_starting_up'] = 'Directory Manomboka ny.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain tsy azo ovana amin&#39;ny fomba andevo.';
+$lang['openldap_generating_configuration'] = 'Niteraka fanahafana ...';
+$lang['openldap_importing_data'] = 'Fanafarana angona ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Fanomanana startup ...';
+$lang['openldap_preparing_system'] = 'Fanomanana system ...';
