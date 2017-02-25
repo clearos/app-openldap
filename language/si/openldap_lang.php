180761243f580128426713a86aedca9c3ab62b53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:27:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'මෙම OpenLDAP ධාවක යෙදුම පරිශීලකයන්, කණ්ඩායම්, ගිණුම් හා වෙනත් බහලුම සේවා සඳහා අවශ්ය මෙවලම් සපයයි.';
+$lang['openldap_app_name'] = 'OpenLDAP ධාවක';
+$lang['openldap_directory_is_offline'] = 'නාමාවලිය නොබැඳි වේ.';
+$lang['openldap_directory_starting_up'] = 'නාමාවලිය දක්වා ආරම්භ කර ඇත.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'වසම් වහල් ක්රමය වෙනස් කළ නොහැක.';
+$lang['openldap_generating_configuration'] = 'වින්යාස ජනන ...';
+$lang['openldap_importing_data'] = 'දත්ත ආයාත ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP සේවාදායකය';
+$lang['openldap_preparing_startup'] = 'ආරම්භක සූදානම් කරමින් ...';
+$lang['openldap_preparing_system'] = 'සූදානම් පද්ධතිය ...';
