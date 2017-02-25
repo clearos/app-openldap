a800aa9cacf000e6e116ea3772de2170910bcb3b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:24:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'ການ app Driver OpenLDAP ໃຫ້ເຄື່ອງມືທີ່ຈໍາເປັນສໍາລັບຜູ້ຊົມໃຊ້, ກຸ່ມ, ການບັນຊີແລະການບໍລິການລະບົບອື່ນໆ.';
+$lang['openldap_app_name'] = 'Driver OpenLDAP';
+$lang['openldap_directory_is_offline'] = 'Directory ອອຟໄລ.';
+$lang['openldap_directory_starting_up'] = 'Directory ແມ່ນເລີ່ມຕົ້ນຂຶ້ນ.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'ໂດເມນບໍ່ສາມາດມີການປ່ຽນແປງໃນຮູບແບບການສໍາລອງ.';
+$lang['openldap_generating_configuration'] = 'ການຜະລິດການຕັ້ງຄ່າ ...';
+$lang['openldap_importing_data'] = 'ການນໍາເຂົ້າຂໍ້ມູນ ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'ການກະກຽມເລີ່ມຕົ້ນ ...';
+$lang['openldap_preparing_system'] = 'ການກະກຽມລະບົບ ...';
