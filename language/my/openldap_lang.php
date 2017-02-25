e240bdeef65e477fc174b7c17884f057d345ddbb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:20:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'အဆိုပါ OpenLDAP ယာဉ်မောင်း app ကိုအသုံးပြုသူများအဖွဲ့များ, အကောင့်များနှင့်အခြား directory ကိုန်ဆောင်မှုများအတွက်လိုအပ်သော tools တွေကိုပေးစွမ်းသည်။';
+$lang['openldap_app_name'] = 'OpenLDAP ယာဉ်မောင်း';
+$lang['openldap_directory_is_offline'] = 'Directory ကိုအော့ဖ်လိုင်းဖြစ်ပါတယ်။';
+$lang['openldap_directory_starting_up'] = 'Directory ကိုစတင်နေပါတယ်။';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'ဒိုမိန်းကျွန် mode မှာပြောင်းလဲသွားတယ်မရနိုင်ပါ။';
+$lang['openldap_generating_configuration'] = 'configuration ကိုထုတ်နေ ...';
+$lang['openldap_importing_data'] = 'ဒေတာများကိုတင်သွင်းခြင်း ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server ကို';
+$lang['openldap_preparing_startup'] = 'startup ပြင်ဆင်နေ ...';
+$lang['openldap_preparing_system'] = 'ပြင်ဆင်နေသည့်စနစ် ...';
