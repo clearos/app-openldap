35eebdada434515ea053c86bfa2f348e80bd25b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:30:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Các ứng dụng điều khiển OpenLDAP cung cấp các công cụ cần thiết cho người dùng, các nhóm, các tài khoản và các dịch vụ thư mục khác.';
+$lang['openldap_app_name'] = 'OpenLDAP điều khiển';
+$lang['openldap_directory_is_offline'] = 'Directory là ẩn.';
+$lang['openldap_directory_starting_up'] = 'Directory được bắt đầu lên.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Tên miền không thể được thay đổi trong chế độ nô lệ.';
+$lang['openldap_generating_configuration'] = 'Tạo cấu hình ...';
+$lang['openldap_importing_data'] = 'Nhập dữ liệu ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP server';
+$lang['openldap_preparing_startup'] = 'Chuẩn bị khởi động ...';
+$lang['openldap_preparing_system'] = 'chuẩn bị hệ thống ...';
