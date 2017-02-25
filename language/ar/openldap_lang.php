880aff0feea2e5ba737b9a91336c215046971c36
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:19:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'ويوفر التطبيق سائق ب OpenLDAP والأدوات اللازمة للمستخدمين والمجموعات والحسابات وخدمات الدليل أخرى.';
+$lang['openldap_app_name'] = 'سائق ب OpenLDAP';
+$lang['openldap_directory_is_offline'] = 'الدليل هو حاليا.';
+$lang['openldap_directory_starting_up'] = 'الدليل هو البدء.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'المجال لا يمكن تغييرها في وضع الرقيق.';
+$lang['openldap_generating_configuration'] = 'توليد التكوين ...';
+$lang['openldap_importing_data'] = 'استيراد البيانات ...';
+$lang['openldap_openldap_server'] = 'ب OpenLDAP الخادم';
+$lang['openldap_preparing_startup'] = 'إعداد بدء التشغيل ...';
+$lang['openldap_preparing_system'] = 'إعداد نظام ...';
