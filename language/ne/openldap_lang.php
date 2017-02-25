c6f6a09e70f7ecfdc24251ea5696c26a47257413
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:26:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'यो OpenLDAP ड्राइभर प्रयोगकर्ताहरू, समूह, खाता र अन्य निर्देशिका सेवाहरू लागि आवश्यक उपकरण प्रदान गर्दछ।';
+$lang['openldap_app_name'] = 'OpenLDAP चालक';
+$lang['openldap_directory_is_offline'] = 'निर्देशिका अफलाइन छ।';
+$lang['openldap_directory_starting_up'] = 'निर्देशिका अप सुरु हुँदैछ।';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'डोमेन दास मोडमा परिवर्तन गर्न सकिँदैन।';
+$lang['openldap_generating_configuration'] = 'कन्फिगरेसन उत्पन्न गर्दै ...';
+$lang['openldap_importing_data'] = 'डाटा आयात गर्दै ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP सर्भर';
+$lang['openldap_preparing_startup'] = 'सुरुवातमा तयारी ...';
+$lang['openldap_preparing_system'] = 'सिस्टम तयारी ...';
