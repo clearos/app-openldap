8591989aa5fe6926faf051b97a845eb3beb40e0d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:19:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'App t&#39;OpenLdap Shofer siguron mjetet e nevojshme për përdoruesit, grupet, llogaritë dhe shërbime të tjera direktoriumeve.';
+$lang['openldap_app_name'] = 'Shofer t&#39;OpenLdap';
+$lang['openldap_directory_is_offline'] = 'Lista e shkëputur.';
+$lang['openldap_directory_starting_up'] = 'Lista është duke u nisur.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain nuk mund të ndryshohet në mënyrë të detyrueshme.';
+$lang['openldap_generating_configuration'] = 'Gjenerimi konfigurimin ...';
+$lang['openldap_importing_data'] = 'Importimin e të dhënave ...';
+$lang['openldap_openldap_server'] = 't&#39;OpenLdap Server';
+$lang['openldap_preparing_startup'] = 'Përgatitja për fillimin e ...';
+$lang['openldap_preparing_system'] = 'sistemit të përgatitur ...';
