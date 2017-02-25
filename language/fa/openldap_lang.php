59f42451177b5c85e8cfa4bce4b9d3682eaac58d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:26:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'برنامه درایور اوپنالدپ برای کاربران، گروه، حساب و سایر خدمات دایرکتوری فراهم می کند ابزار لازم.';
+$lang['openldap_app_name'] = 'درایور اوپنالدپ';
+$lang['openldap_directory_is_offline'] = 'راهنمای آنلاین است.';
+$lang['openldap_directory_starting_up'] = 'راهنمای در راه است تا.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'دامنه می توانید در حالت بنده نمی توان تغییر داد.';
+$lang['openldap_generating_configuration'] = 'تولید پیکربندی ...';
+$lang['openldap_importing_data'] = 'وارد کردن داده ها ...';
+$lang['openldap_openldap_server'] = 'اوپنالدپ سرور';
+$lang['openldap_preparing_startup'] = 'آماده سازی راه اندازی ...';
+$lang['openldap_preparing_system'] = 'آماده سازی سیستم ...';
