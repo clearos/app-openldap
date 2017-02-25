87aebda60638d7eb99f5b0ce689cce261a779a1b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:27:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Aplicația Driver de OpenLDAP oferă instrumentele necesare pentru utilizatori, grupuri, conturi și alte servicii de director.';
+$lang['openldap_app_name'] = 'driver de OpenLDAP';
+$lang['openldap_directory_is_offline'] = 'Director este offline.';
+$lang['openldap_directory_starting_up'] = 'Director pornește.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domeniul nu poate fi modificat în mod slave.';
+$lang['openldap_generating_configuration'] = 'Generatoare de configurare ...';
+$lang['openldap_importing_data'] = 'Se importă date ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Se pregătește de pornire ...';
+$lang['openldap_preparing_system'] = 'Sistem de preparare a ...';
