0c5bc8f347e0db5e14936ffca36c7508969e7787
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:29:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'OpenLDAP Sürücü uygulaması kullanıcıları, gruplar, hesaplar ve diğer dizin hizmetleri için gerekli araçları sağlar.';
+$lang['openldap_app_name'] = 'OpenLDAP Sürücü';
+$lang['openldap_directory_is_offline'] = 'Dizin çevrimdışı.';
+$lang['openldap_directory_starting_up'] = 'Dizin açılıyor.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Alan köle modunda değiştirilemez.';
+$lang['openldap_generating_configuration'] = 'yapılandırmayı oluşturuluyor ...';
+$lang['openldap_importing_data'] = 'veri aktarılıyor ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Sunucu';
+$lang['openldap_preparing_startup'] = 'başlangıç ​​hazırlanıyor ...';
+$lang['openldap_preparing_system'] = 'sistemi hazırlanıyor ...';
