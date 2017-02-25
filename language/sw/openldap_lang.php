75822d69192e64ac812f0011840320e89541fcfb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:28:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'programu OpenLDAP Dereva hutoa zana muhimu kwa ajili ya watumiaji, vikundi, akaunti na huduma nyingine directory.';
+$lang['openldap_app_name'] = 'OpenLDAP dereva';
+$lang['openldap_directory_is_offline'] = 'Saraka ni nje ya mkondo.';
+$lang['openldap_directory_starting_up'] = 'Saraka ni kuanzia juu.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain haiwezi kubadilishwa katika hali ya utumwa.';
+$lang['openldap_generating_configuration'] = 'Kuzalisha Configuration ...';
+$lang['openldap_importing_data'] = 'Kuagiza data ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Maandalizi ya startup ...';
+$lang['openldap_preparing_system'] = 'Kuandaa mfumo wa ...';
