32876a94eb8d4bc32466acab93827f96ada045d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:23:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Soláthraíonn an app Tiománaí OpenLDAP an na huirlisí is gá d&#39;úsáideoirí, grúpaí, cuntais agus seirbhísí eolaire eile.';
+$lang['openldap_app_name'] = 'Tiománaí OpenLDAP';
+$lang['openldap_directory_is_offline'] = 'Tá eolaire as líne.';
+$lang['openldap_directory_starting_up'] = 'Tá eolaire ag tosú suas.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Ní féidir Fearann ​​a athrú i mód daor.';
+$lang['openldap_generating_configuration'] = 'Ag giniúint cumraíocht ...';
+$lang['openldap_importing_data'] = 'Iompórtáil sonraí ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Freastalaí';
+$lang['openldap_preparing_startup'] = 'Ullmhú tosaithe ...';
+$lang['openldap_preparing_system'] = 'Córas Ullmhú ...';
