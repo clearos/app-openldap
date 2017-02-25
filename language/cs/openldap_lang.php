b7a968953ee5596131a143d483b74e5c0d95695c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:20:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Aplikace Driver OpenLDAP poskytuje nezbytné nástroje pro uživatele, skupiny účtů a jiných adresářových služeb.';
+$lang['openldap_app_name'] = 'OpenLDAP Driver';
+$lang['openldap_directory_is_offline'] = 'Adresář je v režimu offline.';
+$lang['openldap_directory_starting_up'] = 'Adresář se spouští.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Doména nelze změnit v režimu slave.';
+$lang['openldap_generating_configuration'] = 'Generování konfiguraci ...';
+$lang['openldap_importing_data'] = 'Import dat ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP server';
+$lang['openldap_preparing_startup'] = 'Příprava uvedení do provozu ...';
+$lang['openldap_preparing_system'] = 'Příprava systému ...';
