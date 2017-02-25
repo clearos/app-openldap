6a3e0a8a5a6ef201b8fd95a87d573012a6a230a3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:23:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'The OpenLDAP Driver app veitir nauðsynlegar verkfæri fyrir notendur, hópa reikninga og önnur skrá þjónustu.';
+$lang['openldap_app_name'] = 'OpenLDAP Driver';
+$lang['openldap_directory_is_offline'] = 'Skrá er ótengdur.';
+$lang['openldap_directory_starting_up'] = 'Skrá er að byrja upp.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Lén ekki hægt að breyta í þræll ham.';
+$lang['openldap_generating_configuration'] = 'Búa stillingar ...';
+$lang['openldap_importing_data'] = 'Flytja gögn ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Undirbúningur gangsetning ...';
+$lang['openldap_preparing_system'] = 'Undirbúningur kerfi ...';
