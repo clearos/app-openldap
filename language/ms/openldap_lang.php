f6808ea51b82da5f4111aedfdc046b30cced08f9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:25:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'The OpenLDAP aplikasi Pemandu menyediakan alat yang diperlukan untuk pengguna, kumpulan, akaun dan perkhidmatan direktori lain.';
+$lang['openldap_app_name'] = 'OpenLDAP pemandu';
+$lang['openldap_directory_is_offline'] = 'Direktori di luar talian.';
+$lang['openldap_directory_starting_up'] = 'Direktori sedang dimulakan.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain tidak boleh diubah dalam mod hamba.';
+$lang['openldap_generating_configuration'] = 'Menjana konfigurasi ...';
+$lang['openldap_importing_data'] = 'Mengimport data ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Menyediakan permulaan ...';
+$lang['openldap_preparing_system'] = 'Menyediakan sistem ...';
