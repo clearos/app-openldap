c58da6acaeafb806b2ea5e6db965b04387773f67
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:25:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Апликацијата за возач OpenLDAP обезбедува потребните алатки за корисници, групи, сметки и други услуги директориум.';
+$lang['openldap_app_name'] = 'OpenLDAP возач';
+$lang['openldap_directory_is_offline'] = 'Директориум не е присутен.';
+$lang['openldap_directory_starting_up'] = 'Директориум на стартување.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Домен не може да се менува во режим на роб.';
+$lang['openldap_generating_configuration'] = 'Генерирање конфигурација ...';
+$lang['openldap_importing_data'] = 'Внесување на податоци ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Подготовка стартување ...';
+$lang['openldap_preparing_system'] = 'систем за подготовка на ...';
