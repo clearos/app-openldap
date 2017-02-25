e8d596603c5fafc44ad2585b65ddb25c47134e71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:24:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'កម្មវិធីកម្មវិធីបញ្ជា OpenLDAP ផ្ដល់នូវឧបករណ៍ចាំបាច់សម្រាប់អ្នកប្រើ, ក្រុមគណនីនិងសេវាថតផ្សេងទៀត។';
+$lang['openldap_app_name'] = 'កម្មវិធីបញ្ជារបស់ OpenLDAP';
+$lang['openldap_directory_is_offline'] = 'ថតគឺក្រៅបណ្ដាញ។';
+$lang['openldap_directory_starting_up'] = 'ថតចាប់ផ្ដើម។';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'ដែនមិនអាចត្រូវបានផ្លាស់ប្តូរនៅក្នុងរបៀបទាសករ។';
+$lang['openldap_generating_configuration'] = 'បង្កើតកំណត់រចនាសម្ព័ន្ធ ...';
+$lang['openldap_importing_data'] = 'ការនាំចូលទិន្នន័យ ...';
+$lang['openldap_openldap_server'] = 'ម៉ាស៊ីនបម្រើរបស់ OpenLDAP';
+$lang['openldap_preparing_startup'] = 'រៀបចំពេលចាប់ផ្ដើម ...';
+$lang['openldap_preparing_system'] = 'ប្រព័ន្ធកំពុងរៀបចំ ...';
