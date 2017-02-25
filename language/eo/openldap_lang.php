3bc3d0dc2010e623f02c0cd19e498525b859c2d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:21:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'La OpenLdap Ŝoforo app provizas la necesan ilojn por uzantoj, grupoj, raportoj kaj aliaj dosierujo servoj.';
+$lang['openldap_app_name'] = 'OpenLdap Ŝoforo';
+$lang['openldap_directory_is_offline'] = 'Dosierujo estas offline.';
+$lang['openldap_directory_starting_up'] = 'Dosierujo funkciigante.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Regado ne povas esti ŝanĝita en sklava maniero.';
+$lang['openldap_generating_configuration'] = 'Generante agordo ...';
+$lang['openldap_importing_data'] = 'Importanta datumojn ...';
+$lang['openldap_openldap_server'] = 'OpenLdap Servilo';
+$lang['openldap_preparing_startup'] = 'Preparante lanĉo ...';
+$lang['openldap_preparing_system'] = 'Preparante sistemo ...';
