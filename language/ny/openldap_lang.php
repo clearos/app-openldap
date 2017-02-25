37e3693260a98a7d5c763f0e731e2d8dc818392b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:20:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'The app OpenLDAP Oyendetsa amapereka zipangizo zofunika owerenga, magulu, nkhani ndi zina Directory.';
+$lang['openldap_app_name'] = 'OpenLDAP Yoyendetsa';
+$lang['openldap_directory_is_offline'] = 'Directory ndi olumikizidwa ku makina.';
+$lang['openldap_directory_starting_up'] = 'Directory ndi muyambitse.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Ankalamulira sizingasinthidwe mu mode kapolo.';
+$lang['openldap_generating_configuration'] = 'Kupoletsa kasinthidwe ...';
+$lang['openldap_importing_data'] = 'Yoti deta ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Kukonzekera oyambitsa ...';
+$lang['openldap_preparing_system'] = 'Kukonzekera dongosolo ...';
