b6cb1c7d7db0dec90768e1ab7362cd8be1f1a6f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:26:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Il-app Sewwieq OpenLDAP jipprovdi l--għodod meħtieġa għall-utenti, gruppi, kontijiet u servizzi tad-direttorju oħrajn.';
+$lang['openldap_app_name'] = 'OpenLDAP sewwieq';
+$lang['openldap_directory_is_offline'] = 'Directory huwa offline.';
+$lang['openldap_directory_starting_up'] = 'Directory huwa bidu.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Dominju ma tistax tinbidel fil-modalità iskjavi.';
+$lang['openldap_generating_configuration'] = 'Generating konfigurazzjoni ...';
+$lang['openldap_importing_data'] = 'Jimporta data ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Tħejjija istartjar ...';
+$lang['openldap_preparing_system'] = 'sistema Tħejjija ...';
