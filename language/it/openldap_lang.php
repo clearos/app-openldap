78a33308bd371a43f04b468b2430c4ea6edb831a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:23:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Il driver App OpenLDAP fornisce gli strumenti necessari per gli utenti, i gruppi, i conti e altri servizi di directory.';
+$lang['openldap_app_name'] = 'driver di OpenLDAP';
+$lang['openldap_directory_is_offline'] = 'Directory non è in linea.';
+$lang['openldap_directory_starting_up'] = 'Directory si sta avviando.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Dominio non può essere cambiato in modalità slave.';
+$lang['openldap_generating_configuration'] = 'Generazione di configurazione ...';
+$lang['openldap_importing_data'] = 'Importazione di dati ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Preparazione di avvio ...';
+$lang['openldap_preparing_system'] = 'Sistema di Preparazione ...';
