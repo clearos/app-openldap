5d25c0b93332a2a0b7bca355f9a080e698a5cea0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:22:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Chofè App a openldap bay zouti yo ki nesesè pou itilizatè, gwoup, kont ak lòt sèvis anyè.';
+$lang['openldap_app_name'] = 'openldap chofè';
+$lang['openldap_directory_is_offline'] = 'Anyè hors.';
+$lang['openldap_directory_starting_up'] = 'Anyè se kòmanse yo.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domèn pa ka chanje nan mòd esklav la.';
+$lang['openldap_generating_configuration'] = 'Génération konfigirasyon ...';
+$lang['openldap_importing_data'] = 'Enpòte done ...';
+$lang['openldap_openldap_server'] = 'openldap sèvè';
+$lang['openldap_preparing_startup'] = 'Preparasyon demaraj ...';
+$lang['openldap_preparing_system'] = 'Preparasyon sistèm ...';
