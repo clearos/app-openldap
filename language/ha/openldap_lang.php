88ae375f4ab5d4b9b55b0e1c1c19017c73a897e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:22:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'The OpenLDAP Driver app bayar da zama dole kayayyakin aiki, don masu amfani, kungiyoyi, asusun da sauran directory ayyuka.';
+$lang['openldap_app_name'] = 'OpenLDAP Driver';
+$lang['openldap_directory_is_offline'] = 'Directory ne offline.';
+$lang['openldap_directory_starting_up'] = 'Directory an fara up.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain ba za a iya canza a bawa mode.';
+$lang['openldap_generating_configuration'] = 'Samar da sanyi ...';
+$lang['openldap_importing_data'] = 'Shigo data ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Shirya farawa ...';
+$lang['openldap_preparing_system'] = 'Shirya tsarin ...';
