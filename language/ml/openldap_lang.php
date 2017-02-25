c1e61466f208a7623c1c17edd20de93d7cfcb7fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:25:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'താളുകളിൽ ഡ്രൈവർ ആപ്പ് ഉപയോക്താക്കൾ, ഗ്രൂപ്പുകൾ, അക്കൌണ്ടുകളും മറ്റ് ഡയറക്ടറി സേവനങ്ങൾ ആവശ്യമായ ഉപകരണങ്ങൾ നൽകുന്നു.';
+$lang['openldap_app_name'] = 'താളുകളിൽ ഡ്രൈവർ';
+$lang['openldap_directory_is_offline'] = 'ഡയറക്ടറി ഓഫ്ലൈൻ ആണ്.';
+$lang['openldap_directory_starting_up'] = 'ഡയറക്ടറി തുടങ്ങുന്ന.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'ഡൊമെയ്ൻ അടിമ മോഡിൽ മാറ്റാനാവില്ല.';
+$lang['openldap_generating_configuration'] = 'കോൺഫിഗറേഷൻ സൃഷ്ടിക്കുന്നു ...';
+$lang['openldap_importing_data'] = 'ഡാറ്റ ഇറക്കുമതി ...';
+$lang['openldap_openldap_server'] = 'താളുകളിൽ സെർവർ';
+$lang['openldap_preparing_startup'] = 'പ്രാരംഭത്തില് തയ്യാറാക്കുന്നു ...';
+$lang['openldap_preparing_system'] = 'സിസ്റ്റം തയ്യാറെടുക്കുന്നു ...';
