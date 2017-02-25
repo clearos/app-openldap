628af39d55f68724a622424880738bb266f81107
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:23:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'The OpenLDAP Driver ngwa na-enye ndị dị mkpa ngwá ọrụ maka ọrụ, dị iche iche, ihe ndekọ na ndị ọzọ na directory ọrụ.';
+$lang['openldap_app_name'] = 'OpenLDAP Driver';
+$lang['openldap_directory_is_offline'] = 'Directory bụ offline.';
+$lang['openldap_directory_starting_up'] = 'Directory na-amalite na elu.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain apụghị gbanwere na ohu mode.';
+$lang['openldap_generating_configuration'] = 'Amụba nhazi ...';
+$lang['openldap_importing_data'] = 'Importing data ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Akwadebe mmalite ...';
+$lang['openldap_preparing_system'] = 'Akwadebe usoro ...';
