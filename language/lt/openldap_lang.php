349d07a607478821aa043d07360350de68783f59
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:24:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'OpenLDAP Vairuotojas programa suteikia būtinų įrankių naudotojams, grupių, sąskaitų ir kitų katalogų paslaugų.';
+$lang['openldap_app_name'] = 'OpenLDAP vairuotojas';
+$lang['openldap_directory_is_offline'] = 'Katalogas yra neprisijungę.';
+$lang['openldap_directory_starting_up'] = 'Katalogas įsijungia.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domeno negali būti keičiamos vergų režimu.';
+$lang['openldap_generating_configuration'] = 'Generatoriniai konfigūraciją ...';
+$lang['openldap_importing_data'] = 'Importuoti duomenis ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Serveris';
+$lang['openldap_preparing_startup'] = 'Pasiruošimas paleidimą ...';
+$lang['openldap_preparing_system'] = 'Pasiruošimas sistemą ...';
