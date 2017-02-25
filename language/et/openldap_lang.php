9809cb61a2950378b985ae2d9e77f744a9948706
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:21:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'OpenLDAP Driver app annab vajalikud vahendid kasutajate, rühmade, raamatupidamise ja teiste kataloog teenuseid.';
+$lang['openldap_app_name'] = 'OpenLDAP Driver';
+$lang['openldap_directory_is_offline'] = 'Kataloog on offline.';
+$lang['openldap_directory_starting_up'] = 'Kataloog käivitub.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domeeni ei saa muuta orjareþiimi.';
+$lang['openldap_generating_configuration'] = 'Genereerimine konfiguratsiooni ...';
+$lang['openldap_importing_data'] = 'Andmete importimine ja ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Ettevalmistused käivitamisel ...';
+$lang['openldap_preparing_system'] = 'Ettevalmistused süsteemi ...';
