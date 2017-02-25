8d942442f9efc1bf7c46907e8f56cbccae487781
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:19:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'The OpenLDAP Վարորդ ծրագիրը ապահովում է անհրաժեշտ գործիքներ օգտվողների համար, խմբերի, հաշիվների եւ այլ ցուցակում:';
+$lang['openldap_app_name'] = 'OpenLDAP Վարորդ';
+$lang['openldap_directory_is_offline'] = 'Ուղեցույց ը հիմա ֆորումում է:';
+$lang['openldap_directory_starting_up'] = 'Ուղեցույց սկսում է.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Տիրույթը չի կարող փոխվել ստրկական ռեժիմում.';
+$lang['openldap_generating_configuration'] = 'Գեներացնող կոնֆիգուրացիան ...';
+$lang['openldap_importing_data'] = 'Importing տվյալները ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Պատրաստվում նորաստեղծ ...';
+$lang['openldap_preparing_system'] = 'Պատրաստվում համակարգ ...';
