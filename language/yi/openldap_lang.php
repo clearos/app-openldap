e0ab2e6989a537f3dcacbdd55080a984c19f56bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:30:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'די אָפּענלדאַפּ דרייווער אַפּ גיט די די נייטיק מכשירים פֿאַר ניצערס, גרופּעס, אַקאַונץ און אנדערע דירעקטארי באַדינונגען.';
+$lang['openldap_app_name'] = 'אָפּענלדאַפּ דרייווער';
+$lang['openldap_directory_is_offline'] = 'Directory איז offline.';
+$lang['openldap_directory_starting_up'] = 'Directory איז סטאַרטינג אַרויף.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'פעלד קענען ניט זיין געביטן אין שקלאַף מאָדע.';
+$lang['openldap_generating_configuration'] = 'גענעראַטינג קאָנפיגוראַטיאָן ...';
+$lang['openldap_importing_data'] = 'ימפּאָרטינג דאַטע ...';
+$lang['openldap_openldap_server'] = 'אָפּענלדאַפּ סערווערס';
+$lang['openldap_preparing_startup'] = 'פּרעפּאַרינג סטאַרטאַפּ ...';
+$lang['openldap_preparing_system'] = 'פּרעפּאַרינג סיסטעם ...';
