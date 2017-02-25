e5e76aa804f9a97770ae7a383ee630e3db63e9b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:29:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Дар барнома Ронанда OpenLDAP барои истифодабарандагон, гурӯҳҳо, ҳисобҳо ва дигар хизматрасониҳои феҳрист таъмин асбобҳои зарурӣ.';
+$lang['openldap_app_name'] = 'ронанда OpenLDAP';
+$lang['openldap_directory_is_offline'] = 'Directory аст.';
+$lang['openldap_directory_starting_up'] = 'Directory сар то.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain метавонад дар ҳолати ғулом карда намешавад тағйир ёфт.';
+$lang['openldap_generating_configuration'] = 'Тавлид танзимоти ...';
+$lang['openldap_importing_data'] = 'Воридоти маълумот ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Омодасозии роҳгузори ...';
+$lang['openldap_preparing_system'] = 'Омодасозии системаи ...';
