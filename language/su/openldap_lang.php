dfcea7b4234caa869b9dbba2d124b99d013ed6ed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:28:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Nu OpenLDAP Supir app nyadiakeun pakakas dipikabutuh pikeun pamaké, kelompok, akun sareng ladenan diréktori séjén.';
+$lang['openldap_app_name'] = 'OpenLDAP Supir';
+$lang['openldap_directory_is_offline'] = 'Diréktori nyaéta offline.';
+$lang['openldap_directory_starting_up'] = 'Diréktori ieu dimimitian up.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain teu bisa robah dina modeu budak.';
+$lang['openldap_generating_configuration'] = 'Generating konfigurasi ...';
+$lang['openldap_importing_data'] = 'Importing data ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'Nyiapkeun ngamimitian ...';
+$lang['openldap_preparing_system'] = 'Nyiapkeun sistim ...';
