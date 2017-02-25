9f4960f1c2b785aeea47bd68bc8d117ddc40a1ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:30:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Mae&#39;r app Gyrwyr OpenLDAP yn darparu&#39;r offer angenrheidiol ar gyfer defnyddwyr, grwpiau, cyfrifon a gwasanaethau cyfeiriadur eraill.';
+$lang['openldap_app_name'] = 'OpenLDAP Driver';
+$lang['openldap_directory_is_offline'] = 'Cyfeirlyfr yn all-lein.';
+$lang['openldap_directory_starting_up'] = 'Cyfeirlyfr yn dechrau i fyny.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Ni all Parth yn cael ei newid yn y modd gaethweision.';
+$lang['openldap_generating_configuration'] = 'Cynhyrchu cyfluniad ...';
+$lang['openldap_importing_data'] = 'Mewnforio data ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Gweinydd';
+$lang['openldap_preparing_startup'] = 'Paratoi startup ...';
+$lang['openldap_preparing_system'] = 'system Paratoi ...';
