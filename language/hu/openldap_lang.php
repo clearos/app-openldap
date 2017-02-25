61831b3270138e64e8f947f27fd0dea931f3a11b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:22:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'Az OpenLDAP Pilóta app biztosítja a szükséges eszközöket a felhasználók, csoportok, számlák és egyéb címtár szolgáltatásokat.';
+$lang['openldap_app_name'] = 'OpenLDAP illesztőprogram';
+$lang['openldap_directory_is_offline'] = 'A könyvtár nem elérhető.';
+$lang['openldap_directory_starting_up'] = 'Directory indul.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Domain nem lehet megváltoztatni a slave módot.';
+$lang['openldap_generating_configuration'] = 'Generálása konfiguráció ...';
+$lang['openldap_importing_data'] = 'Adatok importálása ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP kiszolgáló';
+$lang['openldap_preparing_startup'] = 'Felkészülés indítási ...';
+$lang['openldap_preparing_system'] = 'A rendszer előkészítése ...';
