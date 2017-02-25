b15550e9dfacebcbcc13f21ec37b3e2d7641abf1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:25:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/openldap_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['openldap_app_description'] = 'OpenLDAP Жолооч апп хэрэглэгч, бүлэг, данс, бусад сангийн үйлчилгээнд шаардлагатай багаж хэрэгсэл болно.';
+$lang['openldap_app_name'] = 'OpenLDAP жолооч';
+$lang['openldap_directory_is_offline'] = 'Лавлах офлайн байна.';
+$lang['openldap_directory_starting_up'] = 'Лавлах руу эхэлж байна.';
+$lang['openldap_domain_cannot_be_changed_in_slave_mode'] = 'Домэйн боол горимд өөрчилж чадахгүй.';
+$lang['openldap_generating_configuration'] = 'тохиргоог үүсгэж байна ...';
+$lang['openldap_importing_data'] = 'мэдээллийг импортлож байна ...';
+$lang['openldap_openldap_server'] = 'OpenLDAP Server';
+$lang['openldap_preparing_startup'] = 'эхлүүлэлтийг бэлдэж байна ...';
+$lang['openldap_preparing_system'] = 'системийг бэлдэж байна ...';
